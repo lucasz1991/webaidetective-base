5a12eaf5ef4cdd9a4e05a9ff6cbf362b3428c2f3
Lucas Zacharias <lucas@zacharias-net.de>
2025-07-24 16:33:17 +0200

    12121

--- resources/node/scraper/scrape-instagram.cjs
+++ resources/node/scraper/scrape-instagram.cjs
@@ -1,24 +1,52 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 
 const username = process.argv[2] || 'lcsxzs_zrs'; // Default username if none is provided
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     args: [
       '--no-sandbox',
-      '--disable-setuid-sandbox' 
+      '--disable-setuid-sandbox',
+      '--proxy-server=socks5://shopspaze.com:32769'
     ],
   });
 
   const page = await browser.newPage();
 
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
-
+  // Set screen size to mobile.
+  await page.setViewport({width: 1375, height: 712});
+  //
   await page.goto(`https://www.instagram.com/${username}/`, {
     waitUntil: 'networkidle2',
     timeout: 30000
   });
+
+
+
+
+  const fs = require('fs');
+  const path = require('path');
+
+
+  // Zielpfad vorbereiten
+  const basePath = path.join(__dirname, '../../../storage/app/screenshots', username);
+  const screenshotPath = path.join(basePath, 'profile.png');
+
+  // Ordner erstellen, falls nicht vorhanden
+  fs.mkdirSync(basePath, { recursive: true });
+
+  // Screenshot machen
+  await page.screenshot({
+    path: screenshotPath,
+    fullPage: true
+  });
+
+  console.log('Screenshot gespeichert unter:', screenshotPath);
+
 
   const html = await page.content();
   console.log(html);
