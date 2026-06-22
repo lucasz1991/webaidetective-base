31d62fe6f356905fe2206954d864f433624cee5f
lucas@zacharias-net.de <lucas@zacharias-net.de>
2026-06-22 02:18:57 +0200

    sdsdsd

--- resources/views/components/profile/detail-metrics.blade.php
+++ resources/views/components/profile/detail-metrics.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes->class(['w-full overflow-hidden rounded-3xl border border-white/80 bg-white/90 shadow-sm backdrop-blur']) }}>
-    <div class="grid grid-cols-3 divide-x divide-slate-200 first:rounded-l-3xl last:rounded-r-3xl">
+    <div class="grid grid-cols-3 divide-x divide-slate-200">
         {{ $slot }}
     </div>
 </div>
